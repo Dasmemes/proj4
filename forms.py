cdf663fd9a92d7a64f46848d579791c9ca6b8b5a
dasmemes <kenamugg@gmail.com>
2016-03-11 23:24:58 +0000

    Form Validation on email field

--- forms.py
+++ forms.py
@@ -2,9 +2,10 @@ from flask_wtf import Form
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,Length
 
+
 class SignupForm(Form):
     first_name = StringField("First name", validators=[DataRequired("Please enter your first name")])
     last_name = StringField("Last name", validators=[DataRequired("Please enter your last name")])
     email = StringField("Email", validators=[DataRequired("Please enter your email address"), Email("Please enter a valid email address")])
-    password = PasswordField("Password", validators=[DataRequired("Please enter a password")])
+    password = PasswordField("Password", validators=[DataRequired("Please enter a password"), Length(min=8, message="Password must be at least 8 characters")])
     submit = SubmitField("Sign up", validators=[DataRequired()])
