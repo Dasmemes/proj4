eec51e311934d40e01d783d72eaa6b428e44fcfa
dasmemes <kenamugg@gmail.com>
2016-03-11 23:27:24 +0000

    Added password complexity requirements

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired,Email,Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(Form):
