19904d85f90b4f3c3e83e463805f9cae36668128
dasmemes <kenamugg@gmail.com>
2016-03-12 02:57:21 +0000

    Added database connection and add user feature

--- routes.py
+++ routes.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request
-from models import db
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://localhost/proj4"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@127.0.0.1/proj4'
 db.init_app(app)
 
 app.secret_key = "PzWL1Mrnthy86XhoM60edzcTw99njSeFGaG3KNaDyUCzcxrwqwjQlhFjtHVbEC04"
@@ -24,9 +24,15 @@ def about():
 def signup():
     form = SignupForm()
     if request.method == "POST":
+
         if form.validate() == False:
             return render_template("signup.html", form=form)
-        return "Success!"
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return "Success!"
+
     elif request.method == "GET":
         return render_template("signup.html", form=form)
 
